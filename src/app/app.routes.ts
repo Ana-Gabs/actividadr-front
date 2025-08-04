7d071e4dc2d5024836f43f35175cb0f4be6206e6
Ana-Gabs <2022371069@uteq.edu.mx>
2025-08-03 19:42:14 -0600

    feat: definir rutas para login, home, register y redirección

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: '' }
+];
