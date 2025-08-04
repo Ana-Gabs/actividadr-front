e45dc10d20addfa9d8fc3ba3308fedec9de175cc
Ana-Gabs <2022371069@uteq.edu.mx>
2025-08-03 19:45:27 -0600

    integración de primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/layouts/header/header.component';
+import { FooterComponent } from './shared/layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+ imports: [HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
