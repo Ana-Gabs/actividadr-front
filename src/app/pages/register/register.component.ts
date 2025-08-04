79ec17ad76f3d52397f38a852d7c513e8eae379f
Ana-Gabs <2022371069@uteq.edu.mx>
2025-08-03 21:51:19 -0600

    Agrega componente Registro con estilos y estructura

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, InputTextModule, PasswordModule, ButtonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmarPassword: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario enviado:', this.registerForm.value);
+    }
+  }
 }
